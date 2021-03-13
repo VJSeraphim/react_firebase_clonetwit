38c394cabc638a329f96ec5d2b9bcbc8dc2b4171
VJS <minsung_96@naver.com>
2021-03-13 13:34:45 +0900

    Collection.add hanging and cannot shoot data to the firestore

--- src/myfbase.js
+++ src/myfbase.js
@@ -1,11 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/auth"
+import "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUR_ID
@@ -15,3 +17,4 @@ firebase.initializeApp(firebaseConfig)
 
 export const firebaseInst = firebase
 export const authService = firebase.auth()
+export const dbService = firebase.firestore()
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,32 @@
-import React from 'react'
+import { dbService } from "myfbase"
+import React, {useState} from 'react'
 
-const Home = () => <span>home</span>
+const Home = () => {
+    const [twit, setTwit] = useState("")
+    const onSubmit = async(event) => {
+        event.preventDefault()
+        await dbService.collection("twits").add({
+            twit,
+            createdAt: Date.now()
+        })
+        console.log("update")
+        setTwit("")
+    }
+
+    const onChange = (event) => {
+        const {
+            target : { value }
+        } = event
+        setTwit(value)
+    }
+
+   return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input value= {twit} onChange={onChange} type="text" placeholder="Something on your Mind?" maxLength={120}/>
+            <input type="submit" value="Twit"/>
+        </form>
+    </div>
+   )
+}
 export default Home
