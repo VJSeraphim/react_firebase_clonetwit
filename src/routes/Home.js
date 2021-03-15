cae27609d032cfc1ab6cc1e5bc107768a3b1ad4d
VJS <minsung_96@naver.com>
2021-03-15 22:08:36 +0900

    Get messages, database connection established

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
 import { dbService } from "myfbase"
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
     const [twit, setTwit] = useState("")
+    const [existTwits, setExistTwits] = useState([])
+    const getTwits = async() => {
+        const twits = await dbService.collection("twits").get()
+        twits.forEach((document) => {
+            const twitObj = {
+                ...document.data(),
+                id: document.id,
+            }
+            setExistTwits((prev) => [twitObj, ...prev])
+        })
+    }
+    useEffect (() =>{
+        getTwits()
+    }, [])
     const onSubmit = async(event) => {
         event.preventDefault()
         await dbService.collection("twits").add({
@@ -19,13 +33,18 @@ const Home = () => {
         } = event
         setTwit(value)
     }
-
    return (
     <div>
         <form onSubmit={onSubmit}>
             <input value= {twit} onChange={onChange} type="text" placeholder="Something on your Mind?" maxLength={120}/>
             <input type="submit" value="Twit"/>
         </form>
+        <div>
+            {existTwits.map(twit => 
+            <div key={twit.id}>
+                <h4>{twit.twit}</h4>
+            </div>)}
+        </div>
     </div>
    )
 }
